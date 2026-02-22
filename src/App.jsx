bd296f1fe3f20d3498388e6034f4efc75f53dfb5
ASIMABDULLA <asimabdulla26@gmail.com>
2026-02-22 19:52:45 +0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+# Repo-nu clone edin
+git clone https://github.com/ASIMABDULLA/naxenerjiTwin.git
+cd naxenerjiTwin
+
+# ZIP-dən src/App.jsx faylını kopyalayın
+# Sonra:
+git add .
+git commit -m "Add App.jsx to src"
+git push
